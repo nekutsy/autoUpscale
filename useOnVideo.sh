ebc9fa5af228bf18295f31ececdcdd5125681310
nekutsy <bochkova820@gmail.com>
2023-06-15 21:58:39 +0300

    ~

--- useOnVideo.sh
+++ useOnVideo.sh
@@ -15,9 +15,9 @@ chekOutput() {
     shift
     done
 
-    if [[ -s $path/$name/log.txt ]] ; then
+    if [[ -s $path/log.txt ]] ; then
         echo "undone"
-        echo "see output in $path/$name/log.txt"
+        echo "see output in $path/log.txt"
         exit 1
     else
         if [[ "$quit" != "true" ]] ; then 
@@ -26,18 +26,21 @@ chekOutput() {
     fi
 }
 
-mkdir -p $path/$name 2> /dev/null
-mkdir -p $path/$name/upscaled 2> /dev/null
-mkdir -p $path/$name/segments 2> /dev/null
+mkdir -p $path/upscaled 2> /dev/null
+mkdir -p $path/segments 2> /dev/null
+mkdir -p $tmpPath/inFrames 2> /dev/null
+mkdir -p $tmpPath/processed 2> /dev/null
+mkdir -p $tmpPath/outFrames 2> /dev/null
+mkdir -p $tmpPath/compares 2> /dev/null
 
 begin=0
-if [ -f $path/$name/startFrom.txt ] ; then
-    begin=$(cat $path/$name/startFrom.txt)
+if [ -f $path/startFrom.txt ] ; then
+    begin=$(cat $path/startFrom.txt)
     echo processing continues from fragment $begin
 else
-    rm -rf $path/$name/upscaled/*
-    rm -rf $path/$name/segments.txt
-    rm -rf $path/$name/segments/*
+    rm -rf $path/upscaled/*
+    rm -rf $path/segments.txt
+    rm -rf $path/segments/*
 fi
 rm -rf $tmpPath/inFrames/*
 rm -rf $tmpPath/outFrames/*
@@ -56,32 +59,32 @@ echo
 
 requestedFps=$(printf "%.0f" "$(bc <<< "$fps + 0.5")")
 if [[ "$(printf "%.0f" "$(bc <<< "$fps - 0.5")")" != "$requestedFps" ]] ; then
-    if [[ -f "$path/$name/re-encoded" ]] ; then
+    if [[ -f "$path/re-encoded" ]] ; then
         echo "video already re-encoded into $requestedFps fps"
-        in="$path/$name/$requestedFps$name"
+        in="$path/$requestedFps$name"
         fps=$requestedFps
     else
         echo -n "re-encoding into $requestedFps fps... "
-        ffmpeg -i $in -r 24 -crf 2 $path/$name/$requestedFps$name -v 16 -y 2> $path/$name/log.txt
+        ffmpeg -i $in -r 24 -crf 2 $path/$requestedFps$name -v 16 -y 2> $path/log.txt
         chekOutput
-        touch "$path/$name/re-encoded"
-        in="$path/$name/$requestedFps$name"
+        touch "$path/re-encoded"
+        in="$path/$requestedFps$name"
         fps=$requestedFps
     fi
 fi
 
-segments=($(ls $path/$name/segments))
+segments=($(ls $path/segments))
 len=${#segments[@]}
 
-if [[ -f "$path/$name/segmented" ]] ; then
+if [[ -f "$path/segmented" ]] ; then
     echo "video already segmented"
 else
     echo -n "video segmentation... "
-    $ffmpeg -i $in -c copy -f segment -segment_time $chunkSize "$path/$name/segments/%d.mp4" -v 16 2> $path/$name/log.txt
+    $ffmpeg -i $in -c copy -f segment -segment_time $chunkSize "$path/segments/%d.mp4" -v 16 2> $path/log.txt
     chekOutput
-    segments=($(ls $path/$name/segments))
+    segments=($(ls $path/segments))
     len=${#segments[@]}
-    touch "$path/$name/segmented"
+    touch "$path/segmented"
 fi
 
 echo "segments amount: $len"
@@ -89,31 +92,31 @@ echo
 i=0
 for ((i=$begin; i<$len; i++)) ; do
     #echo "processing of chunk$i"
-    echo $i > $path/$name/startFrom.txt
+    echo $i > $path/startFrom.txt
 
     rm -rf $tmpPath/inFrames/*
     rm -rf $tmpPath/outFrames/*
 
     echo -en "\rdecomposition into frames a segment $i...  "
-    $ffmpeg -i $path/$name/segments/$i.mp4 -qscale:v 1 -vsync 0 $tmpPath/inFrames/\%010d.jpg -hide_banner -v 16 -y 2> $path/$name/log.txt
+    $ffmpeg -i $path/segments/$i.mp4 -qscale:v 1 -vsync 0 $tmpPath/inFrames/\%010d.jpg -hide_banner -v 16 -y 2> $path/log.txt
     chekOutput -n
 
-    ./upscale.sh $i 2> $path/$name/log.txt
+    ./upscale.sh $i 2> $path/log.txt
     chekOutput -q
     echo
 
     echo -n "assembling a segment $i from frames... "
-    $ffmpeg -framerate $fps -i $tmpPath/outFrames/\%010d.jpg -i $path/$name/segments/$i.mp4 -map 0:v -map 1:a -qscale:v 1 $path/$name/upscaled/chunk$i.mp4 -v 16 -y 2> $path/$name/log.txt
+    $ffmpeg -framerate $fps -i $tmpPath/outFrames/\%010d.jpg -i $path/segments/$i.mp4 -map 0:v -map 1:a -qscale:v 1 $path/upscaled/chunk$i.mp4 -v 16 -y 2> $path/log.txt
     chekOutput -n
     
-    echo file upscaled/chunk$i.mp4 >> $path/$name/segments.txt
+    echo file upscaled/chunk$i.mp4 >> $path/segments.txt
 done
-rm -rf $path/$name/startFrom.txt
+rm -rf $path/startFrom.txt
 
 echo
 echo -en "\rassembling a video from segments... "
-$ffmpeg -f concat -safe 0 -i $path/$name/segments.txt -c copy $path/$name/$name -y -v 16 2> $path/$name/log.txt
+$ffmpeg -f concat -safe 0 -i $path/segments.txt -c copy $path/$name -y -v 16 2> $path/log.txt
 chekOutput
 echo -n "deleting video segments... "
-rm -rf $path/$name/segments 2> /dev/null
+rm -rf $path/segments 2> /dev/null
 echo "done"
